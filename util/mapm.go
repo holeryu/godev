12741a3c43b5989040b6a891fcce44c6791af4aa
holeryu@aliyun.com <holeryu@aliyun.com>
2018-07-10 15:44:39 +0800

    补充数据。

--- util/mapm.go
+++ util/mapm.go
@@ -3,5 +3,6 @@ package util
 func CountryCapitalMap() map[string]string {
 	countryCapitalMap := make(map[string]string)
 	countryCapitalMap [ "China" ] = "Beijing"
+	countryCapitalMap [ "U.S.A" ] = "Washington "
 	return countryCapitalMap;
 }

c483c44f1fd7566001d00110abf4a0622588e27c
holeryu@aliyun.com <holeryu@aliyun.com>
2018-07-10 16:12:13 +0800

    补充数据。

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import "github.com/holeryu/godev/util"
 func main() {
 	countryCapitalMap := util.CountryCapitalMap();
 
-	capital,ok := countryCapitalMap [ "China" ]
+	capital,ok := countryCapitalMap [ "U.S.A" ]
 
 	if(ok) {
 		fmt.Println(capital)
